8b0ab93d6e8012bb79fbea21b10b2a678829fa89
Pancho Fernandez <panchofern@gmail.com>
2020-09-04 18:03:06 -0400

    Deploy

--- jobapi/__init__.py
+++ jobapi/__init__.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 CORS(app)
 
 app.config['SECRET_KEY'] = environ.get('MY_SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://wommclqmzcgqsj:21568b5c9f4ccbb2f2efa328e2ce2473b5df13f7b349dc8b50dde2299644dec2@ec2-34-195-115-225.compute-1.amazonaws.com:5432/dde6aqpmnh7uol'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
